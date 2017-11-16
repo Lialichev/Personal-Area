88176d8166a47b97df3d86e4633c1b5563c94a43
Lyalichev <lyalichevv98@gmail.com>
2017-11-16 21:25:42 +0200

    add range slider

--- app/js/init.js
+++ app/js/init.js
@@ -1,30 +1,30 @@
-$( function() {
+$(function () {
 
     //datepicker init
     var dateFormat = "mm/dd/yy",
-        from = $( "#from" )
+        from = $("#from")
             .datepicker({
                 defaultDate: "+1w",
                 changeMonth: true,
                 numberOfMonths: 1
             })
-            .on( "change", function() {
-                to.datepicker( "option", "minDate", getDate( this ) );
+            .on("change", function () {
+                to.datepicker("option", "minDate", getDate(this));
             }),
-        to = $( "#to" ).datepicker({
+        to = $("#to").datepicker({
             defaultDate: "+1w",
             changeMonth: true,
             numberOfMonths: 1
         })
-            .on( "change", function() {
-                from.datepicker( "option", "maxDate", getDate( this ) );
+            .on("change", function () {
+                from.datepicker("option", "maxDate", getDate(this));
             });
 
-    function getDate( element ) {
+    function getDate(element) {
         var date;
         try {
-            date = $.datepicker.parseDate( dateFormat, element.value );
-        } catch( error ) {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
             date = null;
         }
 
@@ -51,20 +51,20 @@ $( function() {
 
 //chart init
     var chartBlock = document.getElementById('myChart');
-    if(chartBlock){
+    if (chartBlock) {
         var myChart = new Chart(chartBlock, {
             type: 'bar',
             data: {
-                labels:['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
                 datasets: [
                     {
                         label: 'Signups',
-                        data: [100,200,220,170,130,90,321,192,234],
+                        data: [100, 200, 220, 170, 130, 90, 321, 192, 234],
                         backgroundColor: 'rgba(128, 191, 202, 1)'
                     },
                     {
                         label: 'FTD',
-                        data: [231,42,241,322,145,290,132,152,289],
+                        data: [231, 42, 241, 322, 145, 290, 132, 152, 289],
                         backgroundColor: 'rgba(255, 154, 56, 1)'
                     }
                 ]
@@ -74,11 +74,42 @@ $( function() {
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero:true
+                            beginAtZero: true
                         }
                     }]
                 }
             }
         })
     }
-} );
+    //range slider init
+    var handle = $('#custom-handle');
+    var range = $('#slider');
+
+    range.slider({
+        value: 50,
+        orientation: "horizontal",
+        range: "min",
+        animate: true,
+        max: 200,
+        create: function (event, ui) {
+            handle.append('<span class="value">' + $(this).slider("value") + 'hours</span>');
+            var value = handle.find('.value');
+            range.slider('option', 'valueSpan', value);
+        },
+        slide: function (event, ui) {
+            range.slider("option", "valueSpan").text(ui.value + 'hours');
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
